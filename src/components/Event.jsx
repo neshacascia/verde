4c5f3e6c509264d16d828d6d792b0135ada356cb
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 15:52:58 -0400

    styled the Event component

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,22 +1,33 @@
 export default function Event({ event, setEventsPosition }) {
   return (
-    <div>
+    <div className="text-center w-[327px]">
       <img src={event.image} />
 
-      <div className="flex flex-col">
-        <button onClick={() => setEventsPosition(0)} className="uppercase">
+      <div className="flex flex-col gap-4 mt-14">
+        <button
+          onClick={() => setEventsPosition(0)}
+          className="text-[#4C4C4C] text-[18px] font-medium tracking-[2.5px] uppercase"
+        >
           Family Gathering
         </button>
-        <button onClick={() => setEventsPosition(1)} className="uppercase">
+        <button
+          onClick={() => setEventsPosition(1)}
+          className="text-[#4C4C4C] text-[18px] font-medium tracking-[2.5px] uppercase"
+        >
           Special Events
         </button>
-        <button onClick={() => setEventsPosition(2)} className="uppercase">
+        <button
+          onClick={() => setEventsPosition(2)}
+          className="text-[#4C4C4C] text-[18px] font-medium tracking-[2.5px] uppercase"
+        >
           Social Events
         </button>
       </div>
 
-      <h2>{event.heading}</h2>
-      <p>{event.description}</p>
+      <h2 className="text-darkBlue text-[32px] font-semibold tracking-wide mt-7 mb-3">
+        {event.heading}
+      </h2>
+      <p className="font-light leading-[26px]">{event.description}</p>
     </div>
   );
 }
