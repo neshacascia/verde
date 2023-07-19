da14de08c1ce9798827cea6f4b42191bb92455a1
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 10:31:58 -0400

    set up an array containing section images, header, and copy

---
+++ data.js
@@ -0,0 +1,14 @@
+export const itemArr = [
+  {
+    id: 0,
+    image: './public/assets/home/mobile/enjoyable-place.jpg',
+    heading: 'Enjoyable place for all the family',
+    text: 'Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.',
+  },
+  {
+    id: 1,
+    image: './public/assets/home/mobile/locally-sourced.jpg',
+    heading: 'The most locally sourced food',
+    text: 'All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.',
+  },
+];
