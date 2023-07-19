f04439b93d7182aec2c073f2eb275af788418a2e
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 11:25:54 -0400

    created new array for storing menu highlights data

--- data.js
+++ data.js
@@ -12,3 +12,27 @@ export const itemArr = [
     text: 'All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.',
   },
 ];
+
+export const menuHighlightsArr = [
+  {
+    id: 0,
+    image: './public/assets/home/mobile/salmon-fillet.png',
+    name: 'Seared Salmon Fillet',
+    description:
+      'Our locally sourced salmon served with a refreshing buckwheat summer salad.',
+  },
+  {
+    id: 1,
+    image: './public/assets/home/mobile/filet-mignon.png',
+    name: 'Rosemary Filet Mignon',
+    description:
+      'Our prime beef served to your taste with a delicious choice of seasonal sides.',
+  },
+  {
+    id: 2,
+    image: './public/assets/home/mobile/chocolate-mousse.png',
+    name: 'Summer Fruit Chocolate Mousse',
+    description:
+      'Creamy mousse combined with summer fruits and dark chocolate shavings.',
+  },
+];
