d4d518118adb8c3307f887961dc89e34d0e2525d
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 13:55:28 -0400

    created an array of objects for storing data for events section

--- data.js
+++ data.js
@@ -38,3 +38,27 @@ export const menuHighlightsArr = [
       'Creamy mousse combined with summer fruits and dark chocolate shavings.',
   },
 ];
+
+export const eventsArr = [
+  {
+    id: 0,
+    image: './public/assets/home/mobile/family-gathering.png',
+    heading: 'Family Gathering',
+    description:
+      'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.',
+  },
+  {
+    id: 1,
+    image: './public/assets/home/mobile/special-events.jpg',
+    heading: 'Special Events',
+    description:
+      'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.',
+  },
+  {
+    id: 2,
+    image: './public/assets/home/mobile/social-events.jpg',
+    heading: 'Social Events',
+    description:
+      'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.',
+  },
+];
