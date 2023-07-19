038f44abba7385d06843a89be64c602e6481dcb9
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 10:33:10 -0400

    mapped over itemArr to render Item component for each element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 import Hero from '../components/Hero';
+import Item from '../components/Item';
+
+import { itemArr } from '../../data';
+
+const items = itemArr.map(item => <Item item={item} key={item.id} />);
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <section className="bg-[#F2F2F2] h-screen">
+      <Hero />
+
+      <section className="bg-[#F2F2F2] h-full">{items}</section>
+    </section>
+  );
 }
