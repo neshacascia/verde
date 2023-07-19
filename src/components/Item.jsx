a9ae1f215be76399c09470cb4091ef2e094cea1a
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 10:38:42 -0400

    added width

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import divide from '../../public/assets/patterns/pattern-divide.svg';
 export default function Item({ item }) {
   console.log(item);
   return (
-    <div className="bg-[#F2F2F2] text-red w-full h-[662px] ">
+    <div className="text-red w-[327px] h-[662px] ">
       <img src={item.image} />
       <img src={divide} />
       <h2>{item.heading}</h2>
