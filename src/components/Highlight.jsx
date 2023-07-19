d433e3260a9c7d92b5095fff65499d714521f73a
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-19 11:33:10 -0400

    created Highlight component and styled it accordingly

---
+++ src/components/Highlight.jsx
@@ -0,0 +1,14 @@
+import line from '../../public/assets/patterns/line.svg';
+
+export default function Highlight({ highlight }) {
+  return (
+    <div className="text-white text-center w-[327px] h-[662px] flex flex-col items-center">
+      <img src={highlight.image} className="mb-2" />
+      <h2 className="text-2xl font-semibold tracking-wider mt-9 mb-3">
+        {highlight.name}
+      </h2>
+      <p className="font-light leading-7 mb-14">{highlight.description}</p>
+      <img src={line} />
+    </div>
+  );
+}
